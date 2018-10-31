ff02af0393ee33e12d8ac3fa60b541d8899dc455
Maria Marco Medina <mariamarcomedina@gmail.com>
2018-10-31 16:36:02 +0100

    Test Full Screen Page Layout with Scroll and jQuery Scroll-to

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".scroll").click(function(event){
+  event.preventDefault();
+  var full_url = this.href;
+  var parts = full_url.split("#");
+  var trgt = parts[1];
+  var target_offset = $("#"+trgt).offset(); 
+  var target_top = target_offset.top;
+ 
+  $('html, body').animate({scrollTop:target_top}, 500);
+});
